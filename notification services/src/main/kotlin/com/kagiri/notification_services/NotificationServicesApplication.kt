c9d47c2d2ab0c788d5b8ae5c77f54500a186e65c
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2023-11-17 02:54:41 +0300

    first commit

---
+++ notification services/src/main/kotlin/com/kagiri/notification_services/NotificationServicesApplication.kt
@@ -0,0 +1,11 @@
+package com.kagiri.notification_services
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotificationServicesApplication
+
+fun main(args: Array<String>) {
+    runApplication<NotificationServicesApplication>(*args)
+}
