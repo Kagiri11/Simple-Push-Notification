d17666067f2775c3589fb7c0bd2a0d9fa83fc3c5
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2023-11-25 00:22:48 +0300

    Adds firebase to app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,11 +51,15 @@ android {
 }
 
 dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
 
+
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-messaging")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.1")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
