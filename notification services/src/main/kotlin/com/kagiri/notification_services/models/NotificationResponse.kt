7a5cabfbd0472aa806ee4ee5bfcfe2b899b8fe9f
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2023-11-24 14:47:06 +0300

    Creates firebase initializer

---
+++ notification services/src/main/kotlin/com/kagiri/notification_services/models/NotificationResponse.kt
@@ -0,0 +1,6 @@
+package com.kagiri.notification_services.models
+
+data class NotificationResponse(
+    val status: Int,
+    val message: String
+)
--- notification services/src/main/kotlin/com/kagiri/notification_services/service/NotificationService.kt
+++ notification services/src/main/kotlin/com/kagiri/notification_services/service/NotificationService.kt
@@ -1,6 +1,37 @@
 package com.kagiri.notification_services.service
 
+import com.google.api.client.util.Value
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import org.slf4j.LoggerFactory
+import org.springframework.core.io.ClassPathResource
+import org.springframework.stereotype.Service
+import java.lang.System.Logger
+import javax.annotation.PostConstruct
+
+@Service
 class NotificationService {
+
+    @Value("\${app.firebase-configuration-file}")
+    private val firebaseConfigPath: String = ""
+
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+
+    @PostConstruct
+    fun initialize() {
+        try {
+            val credentials = GoogleCredentials.fromStream(ClassPathResource(firebaseConfigPath).inputStream)
+            val options = FirebaseOptions.Builder().setCredentials(credentials).build()
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options)
+                logger.info("Firebase application initialized")
+            }
+
+        } catch (e: Exception) {
+            logger.error(e.message)
+        }
+    }
 
     fun sendNotification() {
 
