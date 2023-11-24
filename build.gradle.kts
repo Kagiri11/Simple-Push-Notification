109cb50785ea9e247f7cb12a5a725872cdbf7061
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2023-11-25 00:02:23 +0300

    Adds google services to app

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,14 @@
+
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.0-beta02" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
 }
+
+/*
+dependencies {
+    classpath("com.android.tools.build:gradle:7.0.4")
+    classpath("com.google.gms:google-services:4.3.15")
+}*/
