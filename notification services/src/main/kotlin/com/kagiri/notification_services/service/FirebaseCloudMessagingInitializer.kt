a1e798fc4ad06f935cda3d96f02923a07289d538
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2023-11-30 14:56:12 +0300

    server clean up

--- notification services/src/main/kotlin/com/kagiri/notification_services/service/FirebaseCloudMessagingInitializer.kt
+++ notification services/src/main/kotlin/com/kagiri/notification_services/service/FirebaseCloudMessagingInitializer.kt
@@ -1,35 +1,21 @@
 package com.kagiri.notification_services.service
 
-import com.google.api.client.util.Value
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.core.io.ClassPathResource
-import org.springframework.core.io.Resource
-import org.springframework.core.io.ResourceLoader
 import org.springframework.stereotype.Service
 import java.io.File
 import java.io.FileInputStream
 import javax.annotation.PostConstruct
 
 @Service
-class FirebaseCloudMessagingInitializer(
-) {
-
-   // @Value("\${app.firebase-configuration-file}")
-   // private val firebaseConfigPath: String = ""
-
-    //val resource = resourceLoader.getResource("\${app.firebase-configuration-file}")
-
+class FirebaseCloudMessagingInitializer {
     private val logger = LoggerFactory.getLogger(this.javaClass)
 
     @PostConstruct
     fun initialize() {
         try {
-//            val credentials = GoogleCredentials.fromStream(ClassPathResource(firebaseConfigPath).inputStream)
-            //val credentials = GoogleCredentials.fromStream(resource.inputStream)
             val credentials2 = GoogleCredentials.fromStream(FileInputStream(File("C:\\Users\\Charles Maina\\IdeaProjects\\SimplePushNotification\\notification services\\src\\main\\resources\\firebase-admin-sdk.json")))
             val options = FirebaseOptions.Builder().setCredentials(credentials2).build()
             if (FirebaseApp.getApps().isEmpty()) {
@@ -41,9 +27,4 @@ class FirebaseCloudMessagingInitializer(
             logger.error(e.message)
         }
     }
-
-    fun sendNotification() {
-
-    }
-
 }
